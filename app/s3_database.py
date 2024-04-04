b787766b5ac0f791adbe874b34647829b5b76e6e
ti-sirtec <ti@sirtec.com.br>
2024-04-04 14:03:21 -0300

    :boom:Fix(lista de arquivos): ajustando a listagem dos arquivos

--- app/s3_database.py
+++ app/s3_database.py
@@ -6,36 +6,25 @@ def list_folders_and_files(bucket_name, folder_name=''):
     secret_access_key = os.getenv("SECRET_ACCESS_KEY")
 
     s3 = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+    
+    # Primeiro, listar todas as subpastas dentro de 'Documents/'
     response = s3.list_objects_v2(Bucket=bucket_name, Delimiter='/', Prefix=folder_name)
-
-    folders = []
+    folders = [obj.get('Prefix') for obj in response.get('CommonPrefixes', [])]
+    
     files_dict = {}
-
-    # Listar pastas
-    for obj in response.get('CommonPrefixes', []):
-        folders.append(obj.get('Prefix'))
-        # Inicializar cada pasta no dicionário de arquivos
-        files_dict[obj.get('Prefix')] = []
-
-    # Listar arquivos
-    for obj in response.get('Contents', []):
-        key = obj.get('Key')
-        folder_key = '/'.join(key.split('/')[:-1]) + '/'
-        if folder_key in files_dict:
-            # Se a pasta já existe no dicionário, adicione o arquivo
-            files_dict[folder_key].append({
-                'name': key.split('/')[-1],
-                'url': f"https://{bucket_name}.s3.amazonaws.com/{key}"  #
-            })
-        else:
-            # Caso contrário, crie uma nova entrada para arquivos soltos na raiz (se necessário)
-            if folder_key == folder_name:
-                if folder_name not in files_dict:
-                    files_dict[folder_name] = []
-                files_dict[folder_name].append({
-                    'name': key.split('/')[-1],
-                    'url': f"https://{bucket_name}.s3.amazonaws.com/{key}"  
-                })
-
+    
+    # Para cada subpasta encontrada, listar todos os arquivos
+    for folder in folders:
+        subresponse = s3.list_objects_v2(Bucket=bucket_name, Delimiter='/', Prefix=folder)
+        files = []
+        for obj in subresponse.get('Contents', []):
+            file_key = obj['Key']
+            # Garantir que estamos adicionando apenas arquivos, não subpastas
+            if not file_key.endswith('/'):
+                file_url = f"https://{bucket_name}.s3.amazonaws.com/{file_key}"
+                files.append({'name': file_key.split('/')[-1], 'url': file_url})
+                
+        # Associar os arquivos encontrados com a subpasta correspondente
+        files_dict[folder] = files
 
     return folders, files_dict
