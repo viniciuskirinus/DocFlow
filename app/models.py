32b5db4f68d8273197ea861862dc5da76367936f
ti-sirtec <ti@sirtec.com.br>
2024-04-26 18:11:29 -0300

    :bug:Bugfix(models): ajuste no arquivo de banco

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 import os
 import pymysql
-import logging
 
 # Configurações de conexão
 host = os.getenv("DB_HOST")
@@ -8,26 +7,18 @@ user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 database = os.getenv("DB_NAME")
 
-# Configuração básica de logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
+# Criar uma conexão
 def conectar_db():
     try:
-        # Configurações de conexão
         connection = pymysql.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME"),
-            autocommit=True,
-            cursorclass=pymysql.cursors.DictCursor,
-            connect_timeout=5,
-            read_timeout=30,
-            write_timeout=30,
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            autocommit=True
         )
-        connection.ping(reconnect=True)  # Checa a conexão e tenta reconectar se necessário
-        logging.info("Conexão com o banco de dados estabelecida com sucesso.")
         return connection
+
     except pymysql.Error as err:
-        logging.error(f"Erro ao conectar ao banco de dados: {err}")
+        print(f"Erro de conexão: {err}")
         return None
