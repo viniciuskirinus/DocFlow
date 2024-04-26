1080e96f5daa0f0038da4245100aeec2a20e3847
ti-sirtec <ti@sirtec.com.br>
2024-04-26 17:54:11 -0300

    :boom:Fix(models): ajustada a conexão com o banco

---
+++ app/read_notificatios.py
@@ -0,0 +1,19 @@
+from flask import session
+from .models import conectar_db
+import pymysql.cursors
+
+def marcar_todas_como_lidas():
+    # Obtém a conexão com o banco de dados
+    conexao = conectar_db()
+
+    with conexao.cursor() as cursor:
+        sql = """
+                UPDATE user_notifications
+                SET read = TRUE
+                WHERE id_user = %s
+            """
+        cursor.execute(sql, (session['id_user'],))
+        conexao.commit()
+
+    # Retornando uma string vazia, pois não queremos redirecionar
+    return ''
